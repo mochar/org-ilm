fc936df860737ffde497c4fd0a13fd1a8ac6df2d
Mohammed <mocharrout@gmail.com>
2025-10-29 20:32:55 +0100

    defuddle handle url input

--- scripts/defuddle.mjs
+++ scripts/defuddle.mjs
@@ -6,11 +6,17 @@ import { JSDOM } from 'jsdom';
 
 const baseURL = 'https://example.com/';
 
-const inputPath = process.argv[2];
-const outputFormat = process.argv[3];
-const outputPath = process.argv[4];
+const inputType = process.argv[2];
+const inputPath = process.argv[3];
+const outputFormat = process.argv[4];
+const outputPath = process.argv[5];
 
-const errorFmt = () => console.error('Usage: defuddle.mjs path/to/file.html md/html path/to/output.{md,html}');
+const errorFmt = () => console.error('Usage: defuddle.mjs [url|html] [website.org|path/to/file.html] [markdown|html] (path/to/output.[md|html])');
+
+if (inputType !== "url" && inputType !== "html") {
+  errorFmt();
+  process.exit(1);
+}  
 
 if (!inputPath || !(outputFormat === "markdown" || outputFormat === "html")) {
   errorFmt();
@@ -18,13 +24,21 @@ if (!inputPath || !(outputFormat === "markdown" || outputFormat === "html")) {
 }
 
 try {
-  const html = await readFile(inputPath, 'utf8');
+  const input = inputType === "html"
+      ? await readFile(inputPath, 'utf8')
+      : await JSDOM.fromURL(inputPath);
   const result = await Defuddle(
-  html, baseURL,
-  { markdown: outputFormat == 'markdown', debug: true});
+    input,
+    // inputType === "html" ? baseURL : undefined,
+    inputType === "html" ? baseURL : inputPath,
+    { markdown: outputFormat == 'markdown', debug: false});
 
   if (result?.content) {
-    await writeFile(outputPath, result.content, 'utf8');
+    if (outputPath) {
+      await writeFile(outputPath, result.content, 'utf8');
+    } else {
+      process.stdout.write(result.content);
+    }
   } else {
     console.error('Could not extract readable content.');
     process.exit(1);
